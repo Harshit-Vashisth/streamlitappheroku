b80b325d6a6852c1cfde6fdeefea48020fe2c793
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-29 17:54:46 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+ # gen= st.radio("**Resting Electrocardiographic results**",( 'Normal','Borderline abnormality','Abnormality, include the presence of an arrhythmia'))
+        # if(gen=="Normal"):
+        #     restecg=0
+        # if(gen=="Borderline abnormality"):
+        #     restecg=1
+        # else :
+        #     restecg=2
